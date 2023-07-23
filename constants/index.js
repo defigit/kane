9719196ea97c6d4cbfc6541a1258b1c0ab8d30fc
defigit <93146889+defigit@users.noreply.github.com>
2023-07-23 04:49:36 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x61ec3f025d14cd171c063aa5f77ba8d7f9c63b85";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
